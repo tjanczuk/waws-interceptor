9e5602f6f68fd47016eef0dce44c5af58fc59dc5
Tomasz Janczuk <tomasz@janczuk.org>
2013-01-14 15:55:40 -0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('http').createServer(function (req, res) {
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	console.log('Received request:' + JSON.stringify(req.headers));
 	res.write(JSON.stringify(process.env, null, 2));
 	res.end('CWD: ' + process.cwd());
 }).listen(process.env.PORT || 3000);
